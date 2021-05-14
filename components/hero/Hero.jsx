8aeda5b82caaaa839092427fc9e4ef8088226598
andris.svarcs <andris.svarcs@avanade.com>
2021-05-14 17:45:03 +0100

    Improving parallax animation

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -6,8 +6,8 @@ export const Hero = () => {
   console.log(scrollY);
 
   return (
-    <section className={styles.hero} style={{top: `${-(scrollY*0.0315)}rem`}}>
-      <h1 style={{top: `${-(scrollY*0.005)}rem`, opacity: 1-(scrollY*0.00175)}}>Super smuks uzraksts, rūķiši dzīvo! Tie ir Glāzšķūņa rūķīši</h1>
+    <section className={styles.hero} style={{transform: `translate3d(0, ${-(scrollY*0.05)}px, 0)`}}>
+      <h1 style={{transform: `translate3d(0, ${-(scrollY*0.5)}px, 0)`, opacity: 1-(scrollY*0.00175)}}>Super smuks uzraksts, rūķiši dzīvo! Tie ir Glāzšķūņa rūķīši</h1>
     </section>
   )
 }
