d7259721bda1c0ad8dea1192b91c4194bcb6aace
andris.svarcs <andris.svarcs@avanade.com>
2021-05-24 15:50:15 +0100

    Adding Analytics

--- pages/index.js
+++ pages/index.js
@@ -18,22 +18,34 @@ export default function Home() {
   }, [])
 
   return (
-    <div className={styles.container}>
+    <>
       <Head>
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PJ5PSL4');`
+        }}/>
         <title>Iegādājies māju Jūrmalā</title>
         <meta name="description" content="Pārdodas daudzīvokļu nams Jūrmalā tikai 10 minūtes no jūras"/>
         <link rel="icon" href="/favicon.ico"/>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
-          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&display=swap" rel="stylesheet"/>
       </Head>
 
-      <Hero/>
-      <Map/>
-      <DetailMap/>
-      <FloorPlans/>
-      {load ? <Gallery/> : <p>Loading Gallery ...</p>}
-      <LongStory/>
-      <Footer/>
-    </div>
-)
+      <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PJ5PSL4"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}/>
+      <div className={styles.container}>
+
+        <Hero/>
+        <Map/>
+        <DetailMap/>
+        <FloorPlans/>
+        {load ? <Gallery/> : <p>Loading Gallery ...</p>}
+        <LongStory/>
+        <Footer/>
+      </div>
+    </>
+  )
 }
